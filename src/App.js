5c2664410f4a8d0da09f39ee5bd2d391fe0e5708
Nitin Chandani <nitin1chandani@gmail.com>
2023-03-23 20:05:53 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
+
 function App() {
   const expenses = [
     {
@@ -21,25 +22,12 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+
   return (
-    <>
-      <h1>hello world</h1>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-    </>
+    <div>
+      <h2>Expense Tracker</h2>
+      <Expenses items={expenses} />
+    </div>
   );
 }
 
